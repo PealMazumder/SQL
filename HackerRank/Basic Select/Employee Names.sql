54841b893ebd637afb5e4c5628976b25c448c3eb
PealMazumder <peal6614@gmail.com>
2020-12-14 00:25:55 +0600

    ORDER BY

---
+++ HackerRank/Basic Select/Employee Names.sql
@@ -0,0 +1,8 @@
+/*
+    ==> The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+    ==> To sort the records in ascending order, use the ASC keyword.
+    ==>  To sort the records in descending order, use the DESC keyword.
+*/
+SELECT name
+FROM Employee
+ORDER BY name ASC;
