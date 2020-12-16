e3153d0ea47ef39e83b274ed6069623ec32246f2
PealMazumder <peal6614@gmail.com>
2020-12-16 16:28:49 +0600

    String Concatenation, GROUP BY, ORDER BY, COUNT

---
+++ HackerRank/Advanced Select/The PADS(MySQL).sql
@@ -0,0 +1,19 @@
+/*
+Syntax:
+    SUBSTR(string, start, length)
+    CONCAT() -> string concatenation function
+    The LOWER() function converts a string to lower-case.
+    GROUP BY ->  used to group rows that have the same values
+        "GROUP BY column_name1" is the clause that performs the grouping based on column_name1.
+    ORDER BY ->
+        The ORDER BY keyword is used to sort the result-set in ascending or descending order.
+        The ORDER BY keyword sorts the records in ascending order by default. To sort the records         in descending order, use the DESC keyword.
+*/
+SELECT CONCAT(Name,"(", SUBSTRING(Occupation, 1, 1), ")")
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT("There are a total of ", COUNT(Name), " ", LOWER(Occupation), "s.")
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(Name), Occupation;
